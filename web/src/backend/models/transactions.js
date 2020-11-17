20aaec3d05ad48fe4298208af891105731e1ed2c
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-11-18 01:22:47 +0700

    Create and Get on Transaction
    Haven't Tested + Haven't Validated

---
+++ web/src/backend/models/transactions.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Transactions = mongoose.Schema;
+
+export const TransactionsSchema = new Transactions({
+  receiver: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users"},
+  sender: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users"},
+  assets: [{type: mongoose.Schema.Types.ObjectId, required: true, ref: "Assets"}]
+});
+
+const transactionModel = mongoose.model("Transactions", TransactionsSchema);
+export default transactionModel;
---
+++ web/src/backend/routes/transaction.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { createTransactionController, getTransactionController } from "../controllers/transaction";
+
+const router = express.Router();
+router.post("/", createTransactionController);
+router.get("/:transactionId", getTransactionController);
+module.exports = router;
