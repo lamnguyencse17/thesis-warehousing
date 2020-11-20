0208a4aa7e933a9a5454b3ce06fa9bd0d37b18c0
TranHungKt <tranhung26122612@gmail.com>
2020-11-20 11:34:27 +0700

    Finish #15

---
+++ mobile/frontend/src/common/Utils.js
@@ -0,0 +1,14 @@
+export const UNIT = [
+  'PIECE',
+  'GRAM',
+  'KILOGRAM',
+  'TON',
+  'LITER',
+  'CUBIC METER',
+];
+export const EMAIL_MAX_LENGTH = 50;
+export const PASSWORD_MAX_LENGTH = 32;
+export const PASSWORD_MIN_LENGTH = 6;
+export const NAME_MAX_LENGTH = 50;
+export const ASSET_NAME_MAX_LENGTH = 50;
+export const ASSET_DESCRIPTION_MAX_LENGTH = 100;
