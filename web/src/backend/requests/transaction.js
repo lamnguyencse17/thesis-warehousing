ff82c1485ef6778ef00a23327f721a4e35448d22
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-11-23 23:17:04 +0700

    Finish Transaction Request

---
+++ web/src/backend/requests/transaction.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const createTransferRequest = async ({IDs, newOwner}) => {
+    const url = `${process.env.LEDGER_SERVER}/transfer`;
+    try {
+        const result = await axios.post(url, {IDs, newOwner});
+        // This is temp work because Ledger always return code 200
+        if (result.message != "Success"){
+            return {status: false, message: "Transfer Failed" };
+        }
+        return {status: true};
+    }
+    catch (err){
+        console.log(err);
+        return {
+            status: false,
+            errCode: err.response.status,
+            message: err.response.data.message,
+        };
+    }
+};
