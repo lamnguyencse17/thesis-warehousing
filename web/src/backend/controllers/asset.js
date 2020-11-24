1025f06f1cec656b0b52cad8a89dd6985fc8ebff
TranHungKt <tranhung26122612@gmail.com>
2020-11-24 14:14:22 +0700

    Fix #43

--- web/src/backend/controllers/asset.js
+++ web/src/backend/controllers/asset.js
@@ -2,44 +2,58 @@ import { createAsset, getAssetById } from "../services/asset";
 import { OK_RESPONSE, HANDLED_ERROR_RESPONSE } from "../constants/http";
 import { validateCreateAsset } from "../validators/assetValidator";
 
-import {createAssetRequest} from '../requests/assets'
+import { createAssetRequest } from "../requests/assets";
 export const getAssetController = async (req, res) => {
-    const assetId = req.params.assetId;
-    let { result, status } = await getAssetById(assetId);
-    if (!status) {
-      return res
-        .status(HANDLED_ERROR_RESPONSE)
-        .json({ message: "Something went wrong" });
-    }
-    result = result.toObject();
-    const asset = {...result};
-    return res.status(OK_RESPONSE).json(asset);
+  const assetId = req.params.assetId;
+  let { result, status } = await getAssetById(assetId);
+  if (!status) {
+    return res
+      .status(HANDLED_ERROR_RESPONSE)
+      .json({ message: "Something went wrong" });
+  }
+  result = result.toObject();
+  const asset = { ...result };
+  return res.status(OK_RESPONSE).json(asset);
 };
 
 export const createAssetController = async (req, res) => {
-    const {name, quantity, unit, description} = req.body;
-    let validateResult = validateCreateAsset({name, quantity, unit, description});
-    if (!validateResult.status) {
-      return res
-        .status(HANDLED_ERROR_RESPONSE)
-        .json({ message: validateResult.message });
-    }
-    let { result, status } = await createAsset({name, quantity, unit, description})
-    if (!status) {
-      return res
-        .status(HANDLED_ERROR_RESPONSE)
-        .json({ message: "Something went wrong" });
-    }
-    let ID = result._id
-    let assetRequest= await createAssetRequest({ID, name, quantity, unit, description})
-    if (assetRequest.status == true){
-      result = result.toObject();
-      const asset = {...result};
-      return res.status(OK_RESPONSE).json(asset);
-    }else {
-      return res
-        .status(HANDLED_ERROR_RESPONSE)
-        .json({ message: "Something went wrong" });
-    }
-   
+  const { name, quantity, unit, description } = req.body;
+  let validateResult = validateCreateAsset({
+    name,
+    quantity,
+    unit,
+    description,
+  });
+  if (!validateResult.status) {
+    return res
+      .status(HANDLED_ERROR_RESPONSE)
+      .json({ message: validateResult.message });
+  }
+  let { result, status } = await createAsset({
+    name,
+    quantity,
+    unit,
+    description,
+  });
+  if (!status) {
+    return res
+      .status(HANDLED_ERROR_RESPONSE)
+      .json({ message: "Something went wrong" });
+  }
+  let ID = result._id;
+  let assetRequest = await createAssetRequest({
+    ID,
+    name,
+    quantity,
+    unit,
+    description,
+  });
+  if (assetRequest.status == true) {
+    result = result.toObject();
+    const asset = { ...result };
+    return res.status(OK_RESPONSE).json(asset);
+  }
+  return res
+    .status(HANDLED_ERROR_RESPONSE)
+    .json({ message: "Something went wrong" });
 };
