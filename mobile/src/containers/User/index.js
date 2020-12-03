574893ddf012e842a80c5374d00f73d73e498bf3
Nguyen Quang Lam <46278271+lamnguyencse17@users.noreply.github.com>
2020-12-03 18:03:20 +0700

    Integrating Apollo Client (#99)
    Co-authored-by: TranHungKt <tranhung26122612@gmail.com>

--- mobile/src/containers/User/index.js
+++ mobile/src/containers/User/index.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {useSelector} from 'react-redux';
 import styles from './styles';
 import QRCode from 'react-native-qrcode-svg';
+import {useSubscription, gql} from '@apollo/client';
 
-function User() {
+const query = gql`
+  subscription Subscription {
+    assetCreated {
+      _id
+      name
+    }
+  }
+`;
+
+function User(props) {
+  //EXAMPLE
+  // const [data, setData] = useState([]);
+  // useSubscription(query, {
+  //   onSubscriptionData: ({subscriptionData}) => {
+  //     setData([...data, {...subscriptionData.data}]);
+  //   },
+  // });
   const {_id, name} = useSelector((state) => state.user);
   const [isShowingQR, showQR] = useState(false);
   return (
     <View>
       <View style={styles.title}>
+        {/*{data.map((newData) => (*/}
+        {/*  <Text>{JSON.stringify(newData, null, 2)}</Text>*/}
+        {/*))}*/}
         <TouchableOpacity onPress={() => showQR(true)}>
           <Text style={styles.titleText}>Generate Receiving QR</Text>
         </TouchableOpacity>
---
+++ mobile/src/graphQL/graphQL.js
@@ -0,0 +1,52 @@
+import {split, HttpLink, ApolloClient, InMemoryCache} from '@apollo/client';
+import {getMainDefinition} from '@apollo/client/utilities';
+import {WebSocketLink} from '@apollo/client/link/ws';
+
+const httpLink = new HttpLink({
+  uri: 'http://172.16.5.122:3000/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://172.16.5.122:3000/graphql`,
+  options: {
+    reconnect: true,
+    // connectionParams: {
+    //   authToken: user.authToken,
+    // },
+  },
+});
+
+wsLink.subscriptionClient.on('connecting', () => {
+  console.log('connecting');
+});
+
+wsLink.subscriptionClient.on('connected', () => {
+  console.log('connected');
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({query}) => {
+    const definition = getMainDefinition(query);
+    console.log(
+      'result',
+      definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription',
+    );
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
