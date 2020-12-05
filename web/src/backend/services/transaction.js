0a853fe0fef0b392413adf27cd51177587553b3e
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-05 19:06:55 +0700

    Finished implementing Get History Transaction Of Assets

--- web/src/backend/services/transaction.js
+++ web/src/backend/services/transaction.js
@@ -52,7 +52,11 @@ export const syncTransaction = async ({ _id, receiver, sender, assets }) => {
 	}
 };
 
-export const getTransactionOfSender = async (sender, { limit, offset }, populateInfo) => {
+export const getTransactionOfSender = async (
+	sender,
+	{ limit, offset },
+	populateInfo
+) => {
 	try {
 		const transactions = await transactionModel
 			.find({ sender: mongoose.Types.ObjectId(sender) })
@@ -63,6 +67,21 @@ export const getTransactionOfSender = async (sender, { limit, offset }, populate
 			.populate({ path: "assets", select: populateInfo.assets })
 			.lean();
 		return { status: true, transactions };
+	} catch (err) {
+		return { status: false, message: err };
+	}
+};
+
+export const getTransactionOfAsset = async ({ _id, limit, offset }) => {
+	try {
+		const result = await transactionModel
+			.find({ assets: { $in: [mongoose.Types.ObjectId(_id)] } })
+			.skip(offset)
+			.limit(limit)
+			.populate("sender", "name email")
+			.populate("receiver", "name email")
+			.lean();
+		return { status: true, result };
 	} catch (err) {
 		return { status: false, message: err };
 	}
