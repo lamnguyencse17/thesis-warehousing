ad823d38073dfa2a674a8fcc54f9e80e380d052b
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-06 09:22:12 +0700

    Rework Generate QR Button

--- mobile/android/app/src/main/java/com/frontend/MainApplication.java
+++ mobile/android/app/src/main/java/com/frontend/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactInstanceManager;
