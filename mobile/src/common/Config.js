3506d4364434f87146b5f5bb11182ea0ac753686
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-07 00:39:42 +0700

    Config Sync

--- mobile/src/common/Config.js
+++ mobile/src/common/Config.js
@@ -4,5 +4,5 @@ export default {
   },
   server: 'http://192.168.0.119:3000/api/',
   options: ['Cái', 'Gram', 'Kilogram', 'Tấn', 'Lít', 'Mét Khối'],
-  graphql: 'http://192.168.0.119:3000/api/',
+  graphql: '192.168.0.119:3000',
 };
