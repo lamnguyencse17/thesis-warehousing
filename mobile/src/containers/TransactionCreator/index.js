ccd53fb264d058ba1994ffc3e4fdba530aceae4a
TranHungKt <tranhung26122612@gmail.com>
2020-12-08 19:12:01 +0700

    Fix UI transaction screen

--- mobile/src/containers/TransactionCreator/index.js
+++ mobile/src/containers/TransactionCreator/index.js
@@ -105,7 +105,7 @@ export default class TransactionCreator extends Component {
             </View>
           )}
         </View>
-        <View>
+        <View style={{flex: 0.4}}>
           <Button
             title="Scan Asset QR"
             buttonStyle={styles.scanButton}
