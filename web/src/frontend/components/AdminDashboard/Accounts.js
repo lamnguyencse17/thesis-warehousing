0ad7e009f23521e271f03fd3b57898348688578f
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-22 10:17:52 +0700

    Basic UI Work

--- web/src/frontend/components/AdminDashboard/Accounts.js
+++ web/src/frontend/components/AdminDashboard/Accounts.js
@@ -1,54 +1,33 @@
 import React from "react";
+import AccountRow from "./Account/AccountRow";
 
 export default function Accounts() {
 	return (
-		<div className='p-5 flex-row'>
+		<div className='flex-row p-5'>
 			<div className='flex justify-center'>
 				<h1>ACCOUNT MANAGEMENT</h1>
 			</div>
 			<div
-				className='w-full h-10 grid grid-cols-6 mt-10 px-5 text-center'
+				className='grid w-full h-10 grid-cols-7 px-5 mt-10 text-center'
 				id='Accounts-Header'
 			>
 				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>ID</div>
+					<div className='flex items-center justify-center h-full'>ID</div>
 				</div>
 				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>Name</div>
+					<div className='flex items-center justify-center h-full'>Name</div>
 				</div>
 				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>Email</div>
+					<div className='flex items-center justify-center h-full'>Email</div>
+				</div>
+				<div className='col-span-1 border border-white'>
+					<div className='flex items-center justify-center h-full'>Date</div>
 				</div>
 				<div className='col-span-3 border border-white'>
-					<div className='h-full flex justify-center items-center'>ACTIONS</div>
+					<div className='flex items-center justify-center h-full'>ACTIONS</div>
 				</div>
 			</div>
-			<div className='w-full h-10 grid grid-cols-6 px-5'>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>ID</div>
-				</div>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>Name</div>
-				</div>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>Email</div>
-				</div>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>
-						<button className='w-full h-full bg-green-600'>Edit</button>
-					</div>
-				</div>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>
-						<button className='w-full h-full bg-orange-600'>Rights</button>
-					</div>
-				</div>
-				<div className='col-span-1 border border-white'>
-					<div className='h-full flex justify-center items-center'>
-						<button className='w-full h-full bg-red-600'>Delete</button>
-					</div>
-				</div>
-			</div>
+			<AccountRow />
 		</div>
 	);
 }
--- web/src/frontend/components/AdminDashboard/Assets.js
+++ web/src/frontend/components/AdminDashboard/Assets.js
@@ -1,9 +1,33 @@
 import React from "react";
+import AssetRow from "./Assets/AssetRow";
 
 export default function Assets() {
 	return (
-		<div className='p-5 flex-row'>
-			<div className='flex justify-center'>THIS IS ASSETS</div>
+		<div className='flex-row p-5'>
+			<div className='flex justify-center'>
+				<h1>ASSET MANAGEMENT</h1>
+			</div>
+			<div
+				className='grid w-full h-10 grid-cols-6 px-5 mt-10 text-center'
+				id='Accounts-Header'
+			>
+				<div className='col-span-1 border border-white'>
+					<div className='flex items-center justify-center h-full'>ID</div>
+				</div>
+				<div className='col-span-1 border border-white'>
+					<div className='flex items-center justify-center h-full'>Name</div>
+				</div>
+				<div className='col-span-1 border border-white'>
+					<div className='flex items-center justify-center h-full'>Date</div>
+				</div>
+				<div className='col-span-1 border border-white'>
+					<div className='flex items-center justify-center h-full'>Owner</div>
+				</div>
+				<div className='col-span-2 border border-white'>
+					<div className='flex items-center justify-center h-full'>ACTIONS</div>
+				</div>
+			</div>
+			<AssetRow />
 		</div>
 	);
 }
